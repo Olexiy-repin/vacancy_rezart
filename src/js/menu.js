2b89c974ad35771619c56e1fabe0d73506e33b6a
Olexiy <olexiy@iMacDesign.local>
2018-06-20 16:36:19 +0200

    first commit

--- src/js/menu.js
+++ src/js/menu.js
@@ -11,6 +11,55 @@
 			// $('.vacancy__sale-container').hide(600);
 		});
 	}
+
+	$('.vacancy-title').on('click', () => {
+		$('.vacancy__container').show();
+	});
+
+		$('.vacancy__sale').on('click', () => {
+			$('.vacancy__container').hide();
+			$('.vacancy__sale-container').show();
+		});
+
+		$('.vacancy__finance').on('click', () => {
+			$('.vacancy__container').hide();
+			$('.vacancy__finance-container').show();
+		});
+
+		$('.vacancy__hr').on('click', () => {
+			$('.vacancy__container').hide();
+			$('.vacancy__hr-container').show();
+		});
+
+		$('.vacancy__it').on('click', () => {
+			$('.vacancy__container').hide();
+			$('.vacancy__it-container').show();
+		});
+
+		$('.vacancy__marketing').on('click', () => {
+			$('.vacancy__container').hide();
+			$('.vacancy__marketing-container').show();
+		});
+
+		$('.vacancy__support').on('click', () => {
+			$('.vacancy__container').hide();
+			$('.vacancy__support-container').show();
+		});
+
+		$('.vacancy__legacy').on('click', () => {
+			$('.vacancy__container').hide();
+			$('.vacancy__legal-container').show();
+		});
+
+		$('.vacancy__beginner').on('click', () => {
+			$('.vacancy__container').hide();
+			$('.vacancy__beginner-container').show();
+		});
+
+		let vbCount = $('.vacancy__beginner-container .vacancy__block').length;
+		console.log(vbCount);
+		let vbNumber = $('.vacancy__beginner .vacancy-menu__count').textContent;
+		vbNumber = 'vbCount';
 })();
 
 // (() => {
