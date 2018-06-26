0650e4817ce27fdd6a4452d4e34694139c106d2e
Olexiy <olexiy@iMacDesign.local>
2018-06-26 15:07:11 +0200

    finish fucking tabs

--- src/js/menu.js
+++ src/js/menu.js
@@ -21,37 +21,44 @@
 
 	if (browserWidth >= 1200) {
 		menu.show();
+		$('.vacancy-title').click(function(){
+			$('.vacancy-title').addClass('active');
+			$('.vacancy__tab').removeClass('active');
+			$('.vacancy__container').show(500);
+			$('.vacancy__container').removeClass('active-content');
+			let vacancyBlocks = $('.vacancy__block');
+			let vacancyMoreBlock = $('.vacancy__block-more-than-10');
+
+			vacancyMoreBlock.css('display', 'none');
+
+
+			for (let i = 0; i < vacancyBlocks.length; i++) {
+				if (i > 9) {
+					vacancyBlocks.eq(i).hide();
+				} else {
+					vacancyBlocks.eq(i).show();
+				}
+			}
+
+			if (vacancyBlocks.length < 10) {
+	 		 $('.vacancy__more-btn').css('display', 'none');
+	 	 } else {
+	 		 $('.vacancy__more-btn').css('display', 'inline-block');
+	 	 }
+		});
 	} else {
 		menu.hide();
 	}
 
 // Обработчик вывода табов
-	$('.vacancy-title').click(function(){
-		$('.vacancy-title').addClass('active');
-		$('.vacancy__tab').removeClass('active');
-		$('.vacancy__container').show(500);
-		$('.vacancy__container').removeClass('active-content');
-		let vacancyBlocks = $('.vacancy__block');
-		let vacancyMoreBlock = $('.vacancy__block-more-than-10');
-
-		vacancyMoreBlock.css('display', 'none');
-
-		for (let i = 0; i < vacancyBlocks.length; i++) {
-			if (i > 9) {
-				vacancyBlocks.eq(i).hide();
-			} else {
-				vacancyBlocks.eq(i).show();
-			}
-		}
-	});
-
 	$('.vacancy__tab').click(function(){
 	 $('.vacancy-title').removeClass('active');
 	 $('.vacancy__tab').removeClass('active');
+	 $('.all-vacancy__tab').removeClass('middle-active-tab');
 	 $('.vacancy__container').css('display', 'none');
 	 $(this).addClass('active');
 	 let vacancyContent =  $('#'+$(this).data('id'));
-	 console.log(vacancyContent);
+
 	 vacancyContent.addClass('active-content');
 	 vacancyContent.css('display', 'block');
 
@@ -77,11 +84,11 @@
 $('.vacancy__more-btn').click(function(){
 	let vacancyContainer = $('.vacancy__container');
 	let vacancyTitle = $('.vacancy-title');
+	let allVacancyTab = $('.all-vacancy').find('.vacancy__tab');
 	let vacancyMoreBlocks = $('.vacancy__block-more-than-10');
 
 	for (let i = 0; i < vacancyContainer.length; i++) {
 	 if (vacancyContainer.eq(i).hasClass('active-content')) {
-		 console.log('lol');
 		 let vacancyBlock = vacancyContainer.eq(i).find('.vacancy__block');
 		 let vacancyMoreBlock = vacancyContainer.eq(i).find('.vacancy__block-more-than-10');
 
@@ -153,23 +160,81 @@ $('.vacancy__more-btn').click(function(){
 
 		vacancyTab.on('click', () => {
 			menu.hide(600);
-			
+
 		});
-		// let menu = $('.vacancy-menu');
-		// let sale = $('.vacancy__sale').text();
-		// let vacancyTitle = $('.vacancy-title');
-		// $('.vacancy__sale-container').hide();
-		//
-		// $('.vacancy__sale').on('click', () => {
-		// 	menu.hide(600);
-		// 	vacancyTitle[0].innerHTML = sale;
-		// 	$('.vacancy__sale-container').show(600);
-		// });
-		//
-		// $('.vacancy__all-vacancy').on('click', () => {
-		// 	menu.hide(600);
-		// 	vacancyTitle[0].innerHTML = 'все вакансии';
-		// 	$('.vacancy__sale-container').show(600);
-		// });
+
+		$('.vacancy-menu__item').click(function() {
+
+			let vacancyCount = $(this).find('.vacancy-menu__count');
+			let vacancyName = $(this).find('.vacancy__tab');
+
+			$('.vacancy-title-span').text(vacancyName.text());
+			$('.vacancy-title .vacancy-menu__count').text(vacancyCount.text());
+		});
+
+		$('.all-vacancy__tab').click(function() {
+				$('.all-vacancy__tab').addClass('middle-active-tab');
+				$('.vacancy__tab').removeClass('active');
+				$('.vacancy__container').show(500);
+				$('.vacancy__container').removeClass('active-content');
+				let vacancyBlocks = $('.vacancy__block');
+				let vacancyMoreBlock = $('.vacancy__block-more-than-10');
+
+				vacancyMoreBlock.css('display', 'none');
+
+				for (let i = 0; i < vacancyBlocks.length; i++) {
+					if (i > 9) {
+						vacancyBlocks.eq(i).hide();
+					} else {
+						vacancyBlocks.eq(i).show();
+					}
+				}
+
+				if (vacancyBlocks.length < 10) {
+		 		 $('.vacancy__more-btn').css('display', 'none');
+		 	 } else {
+		 		 $('.vacancy__more-btn').css('display', 'inline-block');
+		 	 }
+		});
+
+		// Обработчик по клику на кнопку
+		$('.vacancy__more-btn').click(function(){
+			let vacancyContainer = $('.vacancy__container');
+			let allVacancyTab = $('.all-vacancy').find('.vacancy__tab');
+			let vacancyMoreBlocks = $('.vacancy__block-more-than-10');
+
+			for (let i = 0; i < vacancyContainer.length; i++) {
+			 if (vacancyContainer.eq(i).hasClass('active-content')) {
+				 let vacancyBlock = vacancyContainer.eq(i).find('.vacancy__block');
+				 let vacancyMoreBlock = vacancyContainer.eq(i).find('.vacancy__block-more-than-10');
+
+				 vacancyMoreBlock.eq(i).toggle(500);
+			 	}
+			 }
+			 if(allVacancyTab.hasClass('middle-active-tab')) {
+				 if(vacancyContainer.hasClass('flag')) {
+					vacancyContainer.removeClass('flag');
+
+					let allVacancyBlocks = $('.vacancy__block');
+					for (let j = 0; j < allVacancyBlocks.length; j++) {
+						if(j>9) {
+							allVacancyBlocks.eq(j).hide(500);
+						}
+					}
+				} else {
+					vacancyContainer.addClass('flag');
+					vacancyContainer.find('.vacancy__block').show(500);
+
+					for (let i = 0; i < vacancyContainer.length; i++) {
+						if(vacancyContainer.eq(i).find('.vacancy__block-more-than-10').length !== 0) {
+							vacancyContainer.eq(i).find('.vacancy__block-more-than-10').show();
+						}
+					}
+				}
+			}
+		});
+
+		let allMiddleVacancy = $('.vacancy__block').length;
+	  $('.all-vacancy .vacancy-menu__count').text(allMiddleVacancy);
 	}
 })();
